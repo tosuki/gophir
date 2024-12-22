4d17051fcc7e061d620cfec31a79d6a34c1698f2
tosuki <okaabe2006@gmail.com>
2024-12-22 16:01:11 -0300

    feat: add authenticate feature in session module

--- server/src/modules/session/AuthUsecase.ts
+++ server/src/modules/session/AuthUsecase.ts
@@ -1,5 +1,51 @@
+import { PrismaClient } from "@prisma/client"
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library"
 
+import { hash } from "bcrypt"
+import { encodeSession } from "./token"
+
+import { Result } from "../../lib/result"
+import { logger } from "../../logger"
 
 export class AuthUsecase {
-    constructor() {}
+    constructor(
+        private prismaClient: PrismaClient
+    ) {}
+
+    public async authenticate(username: string, password: string): Promise<Result<string>> {
+        try {
+            const isUsernameTaken = await this.prismaClient.user.findUnique({
+                where: { username },
+                select: { id: true }
+            })
+
+            if (isUsernameTaken) {
+                return { error: "occupied" }
+            }
+
+            const hashedPassword = await hash(password, 10)
+            const user = await this.prismaClient.user.create({
+                data: {
+                    username,
+                    password: hashedPassword,
+                }
+            })
+
+            const token = encodeSession({
+                id: user.id,
+                username: user.username,
+                createdAt: user.createdAt.getTime(),
+                updatedAt: user.updatedAt.getTime(),
+            })
+
+            return token
+        } catch (err) {
+            if (err instanceof PrismaClientKnownRequestError) {
+                logger.error(`Failed to create the user due to ${err.code} - ${err.message}`)
+                return { error: "database_error" }
+            }
+        
+            return { error: "unhandled" }
+        }
+    }
 }
