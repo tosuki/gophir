ad8523b7d426b7e0f063e7582e23c4f68096c541
tosuki <okaabe2006@gmail.com>
2024-12-25 02:44:45 -0300

    feat: delete client
    The client must be rewritten

--- client/src/api/model/Message.ts
+++
@@ -1,9 +0,0 @@
-export type MessageRaw = {
-    authorId: number,
-    content: string,
-    createdAt: number
-}
-
-export type Message = MessageRaw & {
-    id: number
-}
--- client/src/socket.ts
+++
@@ -1,7 +0,0 @@
-import { io } from "socket.io-client"
-
-const server = io("http://localhost:3333", {
-    autoConnect: true
-})
-
-export default server
