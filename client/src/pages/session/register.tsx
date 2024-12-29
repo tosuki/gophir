d00076d0425755679f53e03ca7ed3b4304af84a3
tosuki <okaabe2006@gmail.com>
2024-12-29 09:13:40 -0300

    feat: add register page

--- client/src/pages/session/register.tsx
+++ client/src/pages/session/register.tsx
@@ -1,10 +1,43 @@
 import { toast } from "react-hot-toast"
+import { register } from "../../services/session"
+
+import { useSession } from "../../hooks/session";
+import { useNavigate } from "react-router";
 
 import { SessionPagePrototype } from "./prototype";
 
 export function RegisterPage() {
+    const session = useSession()
+    const navigate = useNavigate()
+
     const onSubmit = (username: string, password: string) => {
-        toast(`Username: ${username} Password: ${password}`)
+        if (!username || !password) {
+            return toast.error("Please fill all the inputs first")
+        }
+
+        register(username, password)
+            .then((result) => {
+                if (!result.data) {
+                    console.log("Unhandled result of register function", result)
+                    return toast.error("Failed to authenticate, please check the console for more information")
+                }
+
+                session.dispatchers.setPassport(result.data)
+                navigate("/chat")
+            })
+            .catch((registrationError) => {
+                switch (registrationError.error) {
+                    case "conflict":
+                        return toast.error("That email has already been")
+                    case "unauthorized":
+                        return toast.error("Incorrect username or password")
+                    case "not_found":
+                        return toast.error("There is a problem with the server, try again later")
+                    default:
+                        toast.error(`An error occured, try again later (check the console for more information)`)
+                        console.log("Error is ", registrationError)
+                }
+            })
     }
 
     return (
--- client/src/services/session/index.ts
+++ client/src/services/session/index.ts
@@ -4,6 +4,29 @@ import { Result } from "../../lib/result";
 
 import fetcher from "./instance";
 
+export async function register(username: string, password: string): Promise<Result<string>> {
+    try {
+        const { data, status } = await fetcher.post("/register", {
+            username,
+            password
+        })
+
+        if (status !== HttpStatusCode.Created) {
+            throw new Error(`Received ${status} as status code but the client doesn't know how to handle it!`)
+        }
+
+        return { data: data.data.token }
+    } catch (err: any) {
+        if (isAxiosError(err) && !isNetworkError(err)) {
+            throw parseResponseStatusError(err.response?.status)
+        }
+
+        console.log("[Register] Failed to handle the response of the request ", err)
+        return { error: "unhandled" }
+    }
+    
+}
+
 export async function authenticate(username: string, password: string): Promise<Result<string>> {
     try {
         const { data, status } = await fetcher.post("/authenticate", {
