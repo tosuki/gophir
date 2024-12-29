e098a8d2788448b0271b11a0673e44152b718325
tosuki <okaabe2006@gmail.com>
2024-12-29 12:58:51 -0300

    feat: add getById using binary search in message repository

--- server/src/modules/chat/MessageRepository.ts
+++ server/src/modules/chat/MessageRepository.ts
@@ -4,6 +4,7 @@ import { Result } from "../../lib/result"
 
 export interface MessageRepository {
     save(partialMessage: PartialMessage): Result<Message>
+    getById(id: number): Result<Message>
 }
 
 export class InMemoryMessageRepositoryImpl implements MessageRepository {
@@ -23,6 +24,38 @@ export class InMemoryMessageRepositoryImpl implements MessageRepository {
         const highest = this.getHighestId(Array.from(this.values.keys()))
 
         return highest ? highest : 1
+    }
+
+    private getIndexOfMessageByID(
+        id: number,
+        values: number[],
+        low: number = 0,
+        high: number = values.length - 1
+    ): number {
+        const midIndex = Math.round((low + high) / 2)
+        const mid = values[midIndex]
+
+        if (id === mid) {
+            return midIndex
+        }
+
+        if (low > high) {
+            return -1;
+        }
+
+        return mid > id ?
+            this.getIndexOfMessageByID(id, values, low, (midIndex - 1)) :
+            this.getIndexOfMessageByID(id, values, (midIndex + 1), high)
+    }
+
+    public getById(id: number): Result<Message> {
+        const messageIndex = this.getIndexOfMessageByID(id, Array.from(this.values.keys()))
+
+        return {
+            data: messageIndex >= 0 ?
+                this.values.get(messageIndex) :
+                null
+        }
     }
 
     public save(partialMessage: PartialMessage): Result<Message> {
