89be68fce9b5dd296e596fc25b102c6fec899af0
tosuki <okaabe2006@gmail.com>
2025-01-05 17:04:23 -0300

    feat: fix migrations
    I had to add a return to make it work xD

--- migrations/20250105144921_init.ts
+++ migrations/20250105144921_init.ts
@@ -1,19 +1,19 @@
 import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-    const hasTable = knex.schema.hasTable("users")
+    // const hasTable = knex.schema.hasTable("users")
 
-    if (!hasTable) {
-        knex.schema.createTable("users", (table) => {
+    // if (!hasTable) {
+    return knex.schema.createTable("users", (table) => {
             table.increments("id").primary().unique()
             table.string("username").unique().notNullable()
             table.string("password").notNullable()
             table.dateTime("createdAt").notNullable()
         })
-    }
+    // }
 }
 
 
 export async function down(knex: Knex): Promise<void> {
-    knex.schema.dropTableIfExists("users")
+    return knex.schema.dropTableIfExists("users")
 }
