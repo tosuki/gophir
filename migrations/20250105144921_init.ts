3259cb330ae7b082b101b467ae6882588f55717b
tosuki <okaabe2006@gmail.com>
2025-01-05 12:04:20 -0300

    feat: fix users table creation

--- migrations/20250105144921_init.ts
+++ migrations/20250105144921_init.ts
@@ -5,7 +5,7 @@ export async function up(knex: Knex): Promise<void> {
 
     if (!hasTable) {
         knex.schema.createTable("users", (table) => {
-            table.increments()
+            table.increments("id").primary().unique()
             table.string("username").unique().notNullable()
             table.string("password").notNullable()
             table.dateTime("createdAt").notNullable()
