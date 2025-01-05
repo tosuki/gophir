bb3d15dab801c4a829433740b24f3e7456e2d391
tosuki <okaabe2006@gmail.com>
2025-01-05 15:32:55 -0300

    feat: implement critical error on user repository
    I decided to classify the error of user repositories as critical because if something bad happen in the database, the entire application may crash, due to that its obviously a critical error

--- src/repositories/user/UserRepositoryImpl.ts
+++ src/repositories/user/UserRepositoryImpl.ts
@@ -3,6 +3,8 @@ import { Knex } from "knex";
 import type { User } from "src/model/User";
 import type { UserRepository } from "./UserRepository"
 
+import { CriticalError } from "../../library/error/CriticalError"
+
 export class UserRepositoryImpl implements UserRepository {
     private queryBuilder: Knex
     
@@ -11,18 +13,26 @@ export class UserRepositoryImpl implements UserRepository {
     }
 
     async getByUsername(username: string): Promise<User | null> {
-        return this.queryBuilder("users")
-            .where({ username })
-            .first()
+        try {
+            return this.queryBuilder("users")
+                .where({ username })
+                .first()
+        } catch (error: any) {
+            throw new CriticalError("database_error", error.message, error)
+        }
     }
 
     async save(username: string, password: string): Promise<User> {
-        return this.queryBuilder("users")
-            .returning(["id", "username", "password", "createdAt"])
-            .insert({
-                username,
-                password,
-                createdAt: Date.now(),
-            })
+        try {
+            return this.queryBuilder("users")
+                .returning(["id", "username", "password", "createdAt"])
+                .insert({
+                    username,
+                    password,
+                    createdAt: Date.now(),
+                })
+        } catch (error: any) {
+            throw new CriticalError("database_error", error.message, error)
+        }
     }
 }
