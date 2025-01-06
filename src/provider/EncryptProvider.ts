1d9f046c33eeed2704e3a503b1e39d26d4b194f0
tosuki <okaabe2006@gmail.com>
2025-01-05 22:09:52 -0300

    feat: add bcrypt encrypter provider

--- src/provider/EncryptProvider.ts
+++ src/provider/EncryptProvider.ts
@@ -1,16 +1,27 @@
 import { CriticalError } from "../library/error/CriticalError"
+import { hash, compare } from "bcrypt"
 
 export interface EncryptProvider {
-    encrypt(value: string): string
+    encrypt(value: string): Promise<string>
     compare(value: string, encrypted: string): Promise<boolean>
 }
 
 export class BCryptEncryptProvider implements EncryptProvider {
-    encrypt(value: string): string {
-        throw new CriticalError("encrypt_error", "method not implemented yet")
+    private saltRounds: number = 10
+
+    encrypt(value: string): Promise<string> {
+        try {
+            return hash(value, this.saltRounds)
+        } catch (error: any) {
+            throw new CriticalError("encrypt_error", "Failed to encrypt the value", error)
+        }
     }
 
-    async compare(value: string, encrypted: string): Promise<boolean> {
-        return false
+    compare(value: string, encrypted: string): Promise<boolean> {
+        try {
+            return compare(value, encrypted)
+        } catch (error: any) {
+            throw new CriticalError("encrypt_error", "Failed to compare the value with the hash", error)
+        }
     }
 }
