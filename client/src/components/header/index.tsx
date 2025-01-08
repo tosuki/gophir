ef426ba9590f705cf178dc091fb684d400201e28
tosuki <okaabe2006@gmail.com>
2025-01-08 19:09:09 -0300

    feat: add headerbar icons and styles

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -1,11 +1,31 @@
-import { User } from "phosphor-react"
+import { Bell, User } from "phosphor-react"
+
+import { createDropdownMenu } from "../menu/dropdown"
 
 import "./styles.css"
 
 export function Header() {
+    const dropdownMenu = createDropdownMenu([
+        { label: "Profile" },
+        { label: "Settings" },
+        { label: "Exit" }
+    ])
+
     return (
         <div className="header-container">
-            <User color="var(--header-fg)" weight="fill" size={26}/>
+            <Bell 
+                color="var(--header-fg)"
+                weight="fill"
+                size={ 30 }
+                className="bell"
+            />
+            <User
+                onClick={ dropdownMenu.toggleMenu }
+                color="var(--header-fg)"
+                weight="fill"
+                size={30}
+            />
+            { dropdownMenu.Component }
         </div>
     )
 }
---
+++ client/src/components/menu/dropdown/index.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+
+import "./styles.css"
+
+export type DropdownMenuItem = {
+    label: string,
+    icon?: any
+    action?: () => unknown
+}
+
+export function DropdownMenu({ items, isOpen }: {
+    isOpen: boolean,
+    items: DropdownMenuItem[]
+}) {
+    return (
+        <>{ isOpen && (
+            <div className="dropdown-menu-container">
+                <ul>{ items.map((item) => {
+                    return (
+                        <li>{ item.label }</li>
+                    )
+                }) }</ul>
+            </div>
+        )}</>
+    )
+}
+
+export function createDropdownMenu(items: DropdownMenuItem[]) {
+    const [isOpen, setOpen] = useState<boolean>(false)
+
+    return {
+        toggleMenu: () => setOpen(!isOpen),
+        Component: <DropdownMenu 
+            isOpen={ isOpen }
+            items={ items }
+        />
+    }
+}
