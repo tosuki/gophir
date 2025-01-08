ca1b3eafa63af71ee1a8bbf40a55cf3c9733881a
tosuki <okaabe2006@gmail.com>
2025-01-08 14:46:22 -0300

    feat: add messageReceive event

--- src/library/error/ChatError.ts
+++ src/library/error/ChatError.ts
@@ -3,3 +3,7 @@ import { GophirError } from "./Error"
 export class ChatError extends GophirError<
     | "invalid_message_author"
 > {}
+
+export const isChatError = (error: any): error is ChatError => {
+    return error instanceof ChatError
+}
--- src/socket/events/MessageReceiveEvent.ts
+++ src/socket/events/MessageReceiveEvent.ts
@@ -2,12 +2,26 @@ import { SocketEvent } from "../SocketEventHandler"
 import { ChatUseCase } from "../../usecase/chat/ChatUseCase"
 import type { Socket } from "socket.io"
 
+import { logger } from "../../library/logger"
+
+import { isCriticalError } from "../../library/error/CriticalError"
+
 export class MessageReceiveEvent extends SocketEvent <string> {
     constructor(chatUsecase: ChatUseCase) {
         super("messageReceive", chatUsecase)
     }
 
-    execute(socket: Socket, data: string) {
-        throw new Error("Method not implemented.")
+    async execute(socket: Socket, data: string) {
+        try {
+            await this.chatUsecase.sendMessage(data, socket.handshake.auth.passport)
+            return logger.debug(`${socket.session.username} sent: ${data}`)
+        } catch (error: any) {
+            if (!isCriticalError(error)) {
+                logger.debug(`Refused message from ${socket.session.username} due to`, error)
+                return socket.emit("message_error", error)
+            }
+
+            return logger.error(`An error occured when trying to process a message from ${socket.session.username}:`, error)
+        }
     }
 }
