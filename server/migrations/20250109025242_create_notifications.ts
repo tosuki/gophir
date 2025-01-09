249b54a57548fde8db0465153c35edb1ee2f8902
tosuki <okaabe2006@gmail.com>
2025-01-08 23:55:35 -0300

    feat: create a migration to create the notifications table

---
+++ server/migrations/20250109025242_create_notifications.ts
@@ -0,0 +1,19 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable("notifications", (table) => {
+        table.increments("id").primary()
+        table.integer("target").notNullable()
+        table.string("title").notNullable()
+        table.string("body").notNullable()
+        table.datetime("createdAt").defaultTo(knex.fn.now())
+        table.foreign("target").references("id").inTable("users")
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTable("notifications")
+}
+
