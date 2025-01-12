334a2330f839fa03287328e1ea53441d0d81b63a
tosuki <okaabe2006@gmail.com>
2025-01-12 09:54:23 -0300

    feat: add register page

--- client/src/page/session/register.tsx
+++ client/src/page/session/register.tsx
@@ -1,13 +1,30 @@
+import { useNavigate } from "react-router"
 import type { SessionContextProperties } from "../../hooks/session"
+
 import { createSessionPage } from "./prototype"
+import { register } from "../../services/auth"
+
+import { toast } from "react-hot-toast"
 
 export function RegisterPage() {
+    const navigate = useNavigate()
+
     const onSubmit = (
         username: string,
         password: string,
-        session: SessionContextProperties 
+        { setPassport }: SessionContextProperties 
     ) => {
-        console.log(username, password, session)
+        register(username, password).then((result) => {
+            if (!result.error) {
+                setPassport(result.data)
+                return navigate("/")
+            }
+
+            toast.error(result.error.message)
+        }).catch((error) => {
+            console.log(error)
+            toast.error(`Unhandled error occurred, check the console for more information`)
+        })
     }
 
     return createSessionPage({
