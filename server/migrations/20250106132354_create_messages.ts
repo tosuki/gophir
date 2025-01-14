570b417bf98ee2a6a4c440525dc2ef43d39b9b4b
tosuki <okaabe2006@gmail.com>
2025-01-14 10:43:41 -0300

    feat: change the primary key of the messages, also add a new way to get all the messages, but including the author data

--- server/migrations/20250106132354_create_messages.ts
+++ server/migrations/20250106132354_create_messages.ts
@@ -2,7 +2,7 @@ import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable("messages", (table) => {
-        table.increments("id").primary()
+        table.increments("messageId").primary()
         table.string("content").notNullable()
         table.dateTime("createdAt").notNullable().defaultTo(knex.fn.now())
         table.integer("authorId").notNullable()
--- server/src/repositories/message/MessageRepositoryImpl.ts
+++ server/src/repositories/message/MessageRepositoryImpl.ts
@@ -1,6 +1,7 @@
 import { DatabaseProvider } from "../../provider/DatabaseProvider"
 
-import type { Message } from "src/model/Message";
+import type { User } from "../../model/User"
+import type { Message } from "../../model/Message";
 import type { MessageRepository } from "./MessageRepository"
 
 export class MessageRepositoryImpl implements MessageRepository {
@@ -18,10 +19,38 @@ export class MessageRepositoryImpl implements MessageRepository {
     }
 
     getById(id: number): Promise<Message | null> {
-        return this.databaseProvider.findFirst<Message>("messages", { id })
+        return this.databaseProvider.findFirst<Message>("messages", {
+            messageId: id
+        })
     }
 
-    getAll(offset: number, limit: number, includeAuthor?: boolean): Promise<Message[]> {
-        return this.databaseProvider.getAll<Message>("messages", limit, offset)
+
+    private async getAllMessagesWithAuthor(offset: number, limit: number): Promise<(Message & {
+        author?: { id: number, username: string }
+    })[]> {
+        const rawMessages = await this.databaseProvider.getAllWithReference<Message, User>(
+            "messages",
+            "users",
+            "authorId",
+            "id",
+            limit,
+            offset,
+            ["messages.*", "users.id", "users.username"]
+        )
+
+        return rawMessages.map(({ id, username, ...message }) => {
+            return {
+                ...message,
+                author: { id, username },
+            }
+        })
+    }
+
+    getAll(offset: number, limit: number, includeAuthor?: boolean): Promise<(Message & {
+        author?: { id: number, username: string }
+    })[]> {
+        return includeAuthor ? 
+            this.getAllMessagesWithAuthor(offset, limit) :
+            this.databaseProvider.getAll("messages", limit, offset)
     }
 }
