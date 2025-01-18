8d8d4b5c9e2e84281f0abfe47efbea10c973c555
tosuki <okaabe2006@gmail.com>
2025-01-18 10:29:36 -0300

    feat: add profile repository factory

--- server/src/factory/repository.ts
+++ server/src/factory/repository.ts
@@ -2,13 +2,19 @@ import type { DatabaseProvider } from "../provider/DatabaseProvider"
 import type { UserRepository } from "../repositories/user/UserRepository"
 import type { MessageRepository } from "../repositories/message/MessageRepository";
 import type { NotificationRepository } from "../repositories/notification/NotificationRepository"
+import type { ProfileRepository } from "../repositories/profile/ProfileRepository"
 
 import { UserRepositoryImpl } from "../repositories/user/UserRepositoryImpl";
 import { MessageRepositoryImpl } from "../repositories/message/MessageRepositoryImpl";
 import { NotificationRepositoryImpl } from "../repositories/notification/NotificationRepositoryImpl"
+import { ProfileRepositoryImpl } from "../repositories/profile/ProfileRepositoryImpl"
 
 export function createUserRepository(databaseProvider: DatabaseProvider): UserRepository {
     return new UserRepositoryImpl(databaseProvider)
+}
+
+export function createProfileRepository(databaseProvider: DatabaseProvider): ProfileRepository {
+    return new ProfileRepositoryImpl(databaseProvider)
 }
 
 export function createNotificationRepository(databaseProvider: DatabaseProvider): NotificationRepository {
