80177c334802eeffa2f08a925830ba0f9aba14ce
tosuki <okaabe2006@gmail.com>
2025-01-20 18:32:09 -0300

    feat: create profile controller and getProfile method

--- server/src/controller/HttpProfileController.ts
+++ server/src/controller/HttpProfileController.ts
@@ -1,3 +1,23 @@
+import { Request, Response } from "express"
+
+import { ProfileUsecase } from "../usecase/session/ProfileUsecase"
+import { AuthUseCase } from "../usecase/session/AuthUseCase"
+
+import { HttpResponseCode } from "../library/http/HttpResponseCode"
+
 export class HttpProfileController {
+    private profileUsecase: ProfileUsecase
+    private authUsecase: AuthUseCase
+
+    constructor(profileUsecase: ProfileUsecase, authUsecase: AuthUseCase) {
+        this.profileUsecase = profileUsecase
+        this.authUsecase = authUsecase
+    }
+    
+    getProfile(request: Request, response: Response) {
+        response.status(HttpResponseCode.Ok).json({
+            message: "hello world"
+        })
+    }
 
 }
